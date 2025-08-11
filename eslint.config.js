cf52480a763da5a44f2a83ad574179624fd605b6
LsYueh <lsyueh@gmail.com>
2025-08-11 17:38:41 +0800

    產生TS用的宣告檔案

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,12 @@
 const { defineConfig } = require('eslint/config');
 const globals = require('globals');
+const js = require('@eslint/js');
 
 module.exports = defineConfig([
+  { ignores: ["**/node_modules/", "**/dist/"] },
   { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
   { files: ['**/*.{js,mjs,cjs}'], languageOptions: { globals: globals.node } },
+  js.configs.recommended,
   {
     rules: {
       "no-unused-vars": "warn",
---
+++ templar.d.ts
@@ -0,0 +1,15 @@
+import { version } from "./lib/meta";
+/**------+---------+---------+---------+---------+---------+---------+----------
+ * Methods
+---------+---------+---------+---------+---------+---------+---------+--------*/
+/**
+ * parse
+ * @param {string} src
+ */
+export function parse(src: string): void;
+/**
+ * stringify
+ * @param {string} src
+ */
+export function stringify(src: string): void;
+export { version };
